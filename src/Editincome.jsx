8cf75d84beabca2954a388272a52f219d73ac945
NRUBYNATHAN <rubynathan999@gmail.com>
2023-03-16 18:03:43 +0530

    edit

--- src/Editincome.jsx
+++ src/Editincome.jsx
@@ -15,7 +15,7 @@ export function Editincome() {
   
   const [list, setList] = useState(null);
   useEffect(() => {
-    fetch(`${API}/editincome/${id}`)
+    fetch(`${API}/${id}`)
       .then((data) => data.json())
       .then((mvs) => setList(mvs));
   }, [id]);
